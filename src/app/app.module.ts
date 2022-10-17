d235b82d5b1898f1cd155dfb8a520858cf4c6aa4
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-17 15:40:10 -0500

    10-17-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ScurryComponentComponent } from './scurry-component/scurry-component.component';
+
 
 @NgModule({
   declarations: [
@@ -18,3 +18,4 @@ import { ScurryComponentComponent } from './scurry-component/scurry-component.co
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
