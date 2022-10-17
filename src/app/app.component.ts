695d426902fa65c498c9c008de264abfab042e0b
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-17 15:41:32 -0500

    10-17-22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'disgust';
 }
+
+
+
+function scurry() {
+
+  const offsetLeft = Math.random() * (window.innerWidth - this.clientWidth);
+  const offsetTop = Math.random() * (window.innerHeight - this.clientHeight);
+
+  console.log(offsetLeft, offsetTop);
+
+  this.style.top = offsetTop + 'px';
+  this.style.left = offsetLeft + 'px';
+}
+scurry.foreach(button =>)
+button.addEventListener('mouseenter', scurry);
